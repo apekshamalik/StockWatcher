ec37e7861b8e7f442880286b86e05eb513db3ca9
Apeksha Malik <malikapeksha@gmail.com>
2025-04-11 22:40:01 -0400

    Initial Commit

---
+++ StockWatcher/StockWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockWatcherApp.swift
+//  StockWatcher
+//
+//  Created by Apeksha Malik on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct StockWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
